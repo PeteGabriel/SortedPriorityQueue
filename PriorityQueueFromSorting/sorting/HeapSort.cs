fbb3182ac6b17cd53516536ce402c86d27c7657c
PeteGabriel <pedrogabriel@protonmail.ch>
2018-03-07 22:10:12 +0000

    Add first test and first heapsort impl

--- PriorityQueueFromSorting/sorting/HeapSort.cs
+++ PriorityQueueFromSorting/sorting/HeapSort.cs
@@ -1,19 +1,91 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace PriorityQueueFromSorting.sorting
 {
     /// <summary>
-    /// Data structure for efficiently supporting the priority 
-    /// queue operations insert and extract-min. They work by maintaining a partial order 
-    /// on the set of elements which is weaker than the sorted order (so it can be efficient 
-    /// to maintain) yet stronger than random order (so the minimum element can be 
-    /// quickly identified).
+    /// 
     /// </summary>
-    public static class HeapSort<T>
+    public static class HeapSort<T> where T: IComparable<T> 
     {
+        #region PublicApi
+        
+        public static IEnumerable<T> Sort(IEnumerable<T> sequence)
+        {
+            
+        }
+
         public static T[] Sort(T[] sequence)
         {
-            throw new NotImplementedException();
+            var sortedSequence = new T[sequence.Length];
+            CopySequence(sequence, sortedSequence);
+            
+            Heapify(sortedSequence);  
+            ShrinkHeap(sortedSequence);  
+            
+            return sortedSequence;
         }
+
+        #endregion
+        
+        #region private
+
+        private static void Heapify(T[] table)  
+        {  
+            for (int child = 1; child < table.Length; child++)  
+            {  
+                int parent = (child - 1) / 2;  
+                while (parent >= 0 && table[parent].CompareTo(table[child]) < 0)  
+                {  
+                    Swap(table, parent, child);  
+                    child = parent;  
+                    parent = (child - 1) / 2;  
+                }  
+            }  
+        }
+        
+        private static void ShrinkHeap(T[] table)  
+        {  
+            for (int n = table.Length-1; n >= 0; n--)  
+            {  
+                Swap(table, 0, n);  
+                int parent = 0;  
+                while (true)  
+                {  
+                    int leftChild = 2 * parent + 1;  
+                    if (leftChild >= n)  
+                        break; // no more children  
+                    int rightChild = leftChild + 1;  
+                    int maxChild = leftChild;  
+                    if (rightChild < n && table[leftChild].CompareTo(table[rightChild]) < 0)  
+                        maxChild = rightChild;  
+                    if (table[parent].CompareTo(table[maxChild]) < 0)  
+                    {  
+                        Swap(table, parent, maxChild);  
+                        parent = maxChild;  
+                    }  
+                    else  
+                        break; // exit loop  
+                }  
+            }  
+        }  
+        
+        private static void Swap(T[] sequence, int parent, int child)
+        {
+            var temp = sequence[parent];  
+            sequence[parent] = sequence[child];  
+            sequence[child] = temp; 
+        }
+        
+        private static void CopySequence(T[] sequence, T[] newCopy)
+        {
+            for (int i = 0; i < sequence.Length; i++)
+            {
+                newCopy[i] = sequence[i];
+            }
+        }
+        #endregion
+
     }
 }
--- PriorityQueueFromSortingTest/sorting/HeapSortTest.cs
+++ PriorityQueueFromSortingTest/sorting/HeapSortTest.cs
@@ -5,12 +5,11 @@ namespace PriorityQueueFromSortingTest.sorting
 {
     public class BinaryHeapTest
     {
-
         [Fact]
-        public void WhenSortingAlgorithmAppliedToGivenSetOfIntegers_ABinaryHeapShouldBeReturned()
+        public void WhenSortingAlgorithm_AppliedToGivenSetOfIntegers_ExpectBinaryHeap()
         {
-            var input = new int[] { 3 ,2 ,1 ,7 ,8 ,4 ,10 ,16 ,12};
-            var expectedOutput = new int[] {1,2,3,4,7,8,10,12,16};
+            var input = new[] { 3 ,2 ,1 ,7 ,8 ,4 ,10 ,16 ,12};
+            var expectedOutput = new[] {1,2,3,4,7,8,10,12,16};
             var output = HeapSort<int>.Sort(input);
             for (var i = 0; i < output.Length; i++)
             {
